4d6a315a1f07623a33500d684f43a6a4ffb4491b
AndreiPetuch <qqqqq.131wee@yandex.by>
2022-05-24 20:37:34 +0300

    Ceated a new method

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 public class Program {
     public static void main(String[] args) {
+        ArrayList <String> arrayList = new ArrayList<>();
+        arrayList.add("текст один два");
+        arrayList.add("три текст чытыри");
+        arrayList.add("цйуеце уцуцк у цук");
+        System.out.println(getElementsByWorc(arrayList,"текст"));
+
+    }
+    public static ArrayList<String> getElementsByWorc(ArrayList<String> arrayList, String word){
+        return (ArrayList<String>) arrayList.stream().filter(a->a.contains(word)).collect(Collectors.toList());
 
     }
 }
